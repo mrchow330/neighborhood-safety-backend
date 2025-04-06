a418b7e8e39c0404c95feef370d88f6ac421275d
“mrchow330 <echow@mail.bradley.edu>
2025-04-06 17:43:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.get('/', (req, res) => {
 
 // API routes
 app.use('/api/reports', require('./api/reports'));
-// app.use('/api/users', require('./api/users'));
-// app.use('/api/health', require('./api/health'));
+app.use('/api/users', require('./api/users'));
+app.use('/api/health', require('./api/health'));
 
 // Export for Vercel
 module.exports = app;
