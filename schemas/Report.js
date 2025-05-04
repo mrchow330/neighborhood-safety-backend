60ee3e48f81b954595473bf297e5fb66adcbba63
“mrchow330 <echow@mail.bradley.edu>
2025-05-04 16:32:57 -0500

    Updated the report schema

--- schemas/Report.js
+++ schemas/Report.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 // Report Schema
 const reportSchema = new mongoose.Schema({
   report_id: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   issueType: String,
-  // location: {type: String, required: true},
   location: { 
     type: { type: String, enum: ['Point'], required: false }, // GeoJSON type
     coordinates: { type: [Number], required: false }, // [longitude, latitude]
@@ -13,7 +13,6 @@ const reportSchema = new mongoose.Schema({
   photoUri: String,
   createdAt: { type: Date, default: Date.now },
   status: { type: String, default: "Submitted" }, 
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
 // Create a geospatial index on the location field
